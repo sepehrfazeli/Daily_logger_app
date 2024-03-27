10d18354c1f1140044de3f591d6485a94dba47b3
Sepehr Fazeli <59790250+sepehrfazeli@users.noreply.github.com>
2024-03-27 19:10:51 +0100

    add counter to the task that is on going

--- Daily_logger.py
+++ Daily_logger.py
@@ -3,6 +3,8 @@ import datetime
 import os
 from prettytable import PrettyTable
 from colorama import Fore, Style, init
+import threading
+import time
 
 init(autoreset=True)
 
@@ -47,6 +49,17 @@ def start_logging():
     date_str = datetime.datetime.now().strftime('%d_%m_%Y')
     dir_path = os.path.join(os.path.dirname(__file__), date_str)
     last_end_time_file_path = os.path.join(dir_path, 'last_end_time.txt')
+    def display_duration(start_time):
+        time.sleep(1)  # Delay before the first print statement
+        while getattr(threading.current_thread(), "do_run", True):
+            duration = datetime.datetime.now() - start_time
+            total_seconds = duration.total_seconds()
+            hours = int(total_seconds // 3600)
+            minutes = int((total_seconds % 3600) // 60)
+            seconds = int(total_seconds % 60)
+            duration_str = f"{hours:02}:{minutes:02}:{seconds:02}"
+            print(Fore.CYAN + f"\rCurrent duration: {duration_str}", end='')
+            time.sleep(1)
     try:
         if os.path.exists(last_end_time_file_path):  # If the file with the last end time exists
             os.system('cls' if os.name == 'nt' else 'clear')  # Clear console
@@ -59,7 +72,13 @@ def start_logging():
             display_work_log()
             start_time = datetime.datetime.now()
         while True:
-            input(Fore.GREEN + Style.BRIGHT + "Press Enter to end task...")
+            start_time = datetime.datetime.now()
+            duration_thread = threading.Thread(target=display_duration, args=(start_time,), daemon=True)
+            duration_thread.start()
+            print('\n' + Fore.GREEN + Style.BRIGHT + "Press Enter to end task...")
+            input()
+            duration_thread.do_run = False
+            duration_thread.join()
             os.system('cls' if os.name == 'nt' else 'clear')  # Clear console
             display_work_log()
             input(Fore.RED + Style.BRIGHT + "Press Enter again to confirm...")
